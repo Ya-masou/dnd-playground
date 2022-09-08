8ade46a13f5c58d98e6200b5839af5ad2b68743b
Yamasou <s.yama@ashita-team.com>
2022-09-08 11:18:49 +0900

    react-beautiful-dnd 試してみた
    * Next.js だと SSR の関係でうまく動作しなかった
    * 色々追加で設定なりコードなりが必要そう

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { resetServerContext } from "react-beautiful-dnd";
+import type { DocumentContext } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    resetServerContext();
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
